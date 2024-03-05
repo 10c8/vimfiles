cf5727cf58f093e6855bf19575d811bad6ff3866
10c8 <2660859+10c8@users.noreply.github.com>
2024-03-05 04:32:49 -0300

    fix: leap safe labels
    - Also enabled `relative_directions` so we can keep using a leap motion even after an operation

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,10 +4,14 @@ return {
   config = function()
     local leap = require 'leap'
 
+    leap.opts.safe_labels = 'fnut/SFNLHMUGTZ?'
+
     leap.opts.special_keys.prev_target = '<bs>'
     leap.opts.special_keys.prev_group = '<bs>'
 
-    require('leap.user').set_repeat_keys('<cr>', '<bs>')
+    require('leap.user').set_repeat_keys('<cr>', '<bs>', {
+      relative_directions = true,
+    })
 
     -- [[ Keymaps ]]
     vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap-forward)', { desc = 'Leap forward' })
